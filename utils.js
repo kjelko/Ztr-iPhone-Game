47bd15cf73cdcc354704ce8c8e0ca433c7dc718d
Kevin Elko <kjelko@gmail.com>
2012-04-07 15:17:59 -0400

    Updating sat collision function

--- utils.js
+++ utils.js
@@ -40,19 +40,20 @@ utils.square = function(x, y, width, height) {
  * @param {int} angle
  * @param {Object} The polygon to rotate.
  * @return {Object} An polygon object with updated points.
+ * 
  */
 utils.rotatePoly = function(angle, poly) {
   var cos = Math.cos(-1*angle),
   sin = Math.sin(-1*angle),
-  poly2 = [];
+  poly1 = [];
   
   for(i = 0; i < poly.length; i++){
-    poly2[i] = [];
-    poly2[i][0] = cos * poly[i][0] - sin * poly[i][1];
-    poly2[i][1] = cos * poly[i][1] + sin * poly[i][0];
+    poly1[i] = [];
+    poly1[i][0] = cos * poly[i][0] - sin * poly[i][1];
+    poly1[i][1] = cos * poly[i][1] + sin * poly[i][0];
   }
   
-  return poly2;
+  return poly1;
 };
 
 
@@ -61,135 +62,97 @@ utils.rotatePoly = function(angle, poly) {
  * @param {Object} poly1 Polygon object to check. 
  * @param {Object} poly2 Polygon object to check.
  * @return {Object} Returns an object with information about the collision.
- * 
+ *
  */
-utils.sat = function(poly1, poly2) {
-    var points1 = poly1,
-			points2 = poly2,
-			i = 0, l = points1.length,
-			j, k = points2.length,
-			normal = {x: 0, y: 0},
-			length,
-			min1, min2,
-			max1, max2,
-			interval,
-			MTV = null,
-			MTV2 = null,
-			MN = null,
-      MN2 = null,
-			dot,
-			nextPoint,
-			currentPoint;
-		
-		//loop through the edges of Polygon 1
-		for(;i<l;i++) {
-			nextPoint = points1[(i==l-1 ? 0 : i+1)];
-			currentPoint = points1[i];
-			
-			//generate the normal for the current edge
-			normal.x = -(nextPoint[1] - currentPoint[1]);
-			normal.y = (nextPoint[0] - currentPoint[0]);
-			
-			//normalize the vector
-			length = Math.sqrt(normal.x * normal.x + normal.y * normal.y);
-			normal.x /= length;
-			normal.y /= length;
-			
-			//default min max
-			min1 = min2 = -1;
-			max1 = max2 = -1;
-			
-			//project all vertices from poly1 onto axis
-			for(j = 0; j < l; ++j) {
-				dot = points1[j][0] * normal.x + points1[j][1] * normal.y;
-				if(dot > max1 || max1 === -1) max1 = dot;
-				if(dot < min1 || min1 === -1) min1 = dot;
-			}
-			
-			//project all vertices from poly2 onto axis
-			for(j = 0; j < k; ++j) {
-				dot = points2[j][0] * normal.x + points2[j][1] * normal.y;
-				if(dot > max2 || max2 === -1) max2 = dot;
-				if(dot < min2 || min2 === -1) min2 = dot;
-			}
-			
-			//calculate the minimum translation vector should be negative
-			if(min1 < min2) {
-			    interval = min2 - max1;
-				
-				normal.x = -normal.x;
-			    normal.y = -normal.y;
-			} else {
-			    interval = min1 - max2;			   
-			}
-			
-			//exit early if positive
-			if(interval >= 0) {
-				return false;
-			}
-			
-			if(MTV === null || interval > MTV) {
-				MTV = interval;
-				MN = {x: normal.x, y: normal.y};
-			}
+utils.sat = function(poly1, poly2){
+  var mtv, mtv2, norm1, norm2, current, next,
+  dot, overlap, min1, max1, min2, max2, magnitude, norm = {};
+  
+  for(i = 0; i < poly1.length; i++) {
+    current = poly1[i],
+    next = poly1[(i == poly1.length-1) ? 0 : i+1];
+    
+    norm.x = -(next[1] - current[1]);
+    norm.y = (next[0] - current[0]);
+    magnitude = Math.sqrt(norm.x*norm.x + norm.y*norm.y);
+    norm.x /= magnitude;
+    norm.y /= magnitude;
+    
+    min1 = min2 = max1 = max2 = -1;
+    
+		for(j = 0; j < poly1.length; ++j) {
+		  dot = poly1[j][0] * norm.x + poly1[j][1] * norm.y;
+			if(dot > max1 || max1 == -1) max1 = dot;
+			if(dot < min1 || min1 == -1) min1 = dot;
 		}
-		
-		//loop through the edges of Polygon 2
-		for(i=0;i<k;i++) {
-			nextPoint = points2[(i==k-1 ? 0 : i+1)];
-			currentPoint = points2[i];
-			
-			//generate the normal for the current edge
-			normal.x = -(nextPoint[1] - currentPoint[1]);
-			normal.y = (nextPoint[0] - currentPoint[0]);
-			
-			//normalize the vector
-			length = Math.sqrt(normal.x * normal.x + normal.y * normal.y);
-			normal.x /= length;
-			normal.y /= length;
-			
-			//default min max
-			min1 = min2 = -1;
-			max1 = max2 = -1;
-			
-			//project all vertices from poly1 onto axis
-			for(j = 0; j < l; ++j) {
-				dot = points1[j][0] * normal.x + points1[j][1] * normal.y;
-				if(dot > max1 || max1 === -1) max1 = dot;
-				if(dot < min1 || min1 === -1) min1 = dot;
-			}
-			
-			//project all vertices from poly2 onto axis
-			for(j = 0; j < k; ++j) {
-				dot = points2[j][0] * normal.x + points2[j][1] * normal.y;
-				if(dot > max2 || max2 === -1) max2 = dot;
-				if(dot < min2 || min2 === -1) min2 = dot;
-			}
-			
-			//calculate the minimum translation vector should be negative
-			if(min1 < min2) {
-			    interval = min2 - max1;
-				  normal.x = -normal.x;
-			    normal.y = -normal.y;
-			} else {
-			    interval = min1 - max2;
-			}
-			
-			//exit early if positive
-			if(interval >= 0) {
-				return false;
-			}
-			
-			if(interval > MTV2 || MTV2 === null) {
-				MTV2 = interval;
-				MN2 = {x: normal.x, y: normal.y};
-			}
+    
+    for(j = 0; j < poly2.length; ++j) {
+			dot = poly2[j][0] * norm.x + poly2[j][1] * norm.y;
+			if(dot > max2 || max2 == -1) max2 = dot;
+		  if(dot < min2 || min2 == -1) min2 = dot;
 		}
-		if(Math.abs(MTV) < Math.abs(MTV2))
-    return {overlap: MTV, normal: MN};
-    else
-    return {overlap: MTV2, normal: MN2};
-	
+    
+    if(min1 < min2) {
+      overlap = min2 - max1;
+      norm.x *= -1;
+      norm.y *= -1;
+    } else {
+      overlap = min1 - max2;
+    }
+    
+    if(overlap >= 0) return false;
+    
+    if(mtv == undefined || overlap > mtv){
+      mtv = overlap;
+      norm1 = {x: norm.x, y: norm.y};
+    }
+    
   }
-
-
+  
+  for(i = 0; i < poly2.length; i++) {
+    current = poly2[i],
+    next = poly2[(i == poly2.length-1) ? 0 : i+1];
+    
+    norm.x = -(next[1] - current[1]);
+    norm.y = (next[0] - current[0]);
+    magnitude = Math.sqrt(norm.x*norm.x + norm.y*norm.y);
+    norm.x /= magnitude;
+    norm.y /= magnitude;
+    
+    min1 = min2 = max1 = max2 = -1;
+    
+		for(j = 0; j < poly1.length; ++j) {
+		  dot = poly1[j][0] * norm.x + poly1[j][1] * norm.y;
+			if(dot > max1 || max1 == -1) max1 = dot;
+			if(dot < min1 || min1 == -1) min1 = dot;
+		}
+    
+    for(j = 0; j < poly2.length; ++j) {
+			dot = poly2[j][0] * norm.x + poly2[j][1] * norm.y;
+			if(dot > max2 || max2 == -1) max2 = dot;
+		  if(dot < min2 || min2 == -1) min2 = dot;
+		}
+    
+    if(min1 < min2) {
+      overlap = min2 - max1;
+      norm.x *= -1;
+      norm.y *= -1;
+    } else {
+      overlap = min1 - max2;
+    }
+    
+    if(overlap >= 0) return false;
+    
+    if(mtv2 == undefined || overlap > mtv2){
+      mtv2 = overlap;
+      norm2 = {x: norm.x, y: norm.y};
+    }
+    
+  }
+  
+  if(Math.abs(mtv) < Math.abs(mtv2))
+    return {overlap: mtv, normal: norm1};
+  else
+    return {overlap: mtv2, normal: norm2}
+  
+};
